7b9f1ea41adebf750ec09bfc191f7caeaa0086dd
unkchandev <unkchandev@gmail.com>
2017-04-05 18:29:53 +0900

    add save/load config file

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"bufio"
 	"io/ioutil"
 	"log"
+	"os"
 
 	"github.com/lxn/walk"
 	. "github.com/lxn/walk/declarative"
@@ -11,8 +13,12 @@ import (
 
 type MyMainWindow struct {
 	*walk.MainWindow
+	settings Setting
 
-	logTE *walk.TextEdit
+	nameEdit *walk.LineEdit
+	fromEdit *walk.LineEdit
+	toEdit   *walk.LineEdit
+	nameBox  *walk.ComboBox
 }
 
 type Item struct {
@@ -24,24 +30,40 @@ type Setting struct {
 	Items []*Item `yaml:"Items"`
 }
 
-func getSettings() []*Item {
+func (mw *MyMainWindow) getSettings() []*Item {
 	buf, err := ioutil.ReadFile("config.yml")
 	if err != nil {
 		return nil
 	}
 
+	log.Println(string(buf))
 	var s Setting
 	if err := yaml.Unmarshal(buf, &s); err != nil {
 		return nil
 	}
+	log.Println(s)
+	mw.settings = s
 	return s.Items
 }
 
+func (mw *MyMainWindow) setSettings() {
+	for i, v := range mw.settings.Items {
+		if v.Name == mw.nameBox.Text() {
+			mw.fromEdit.SetText(mw.settings.Items[i].From)
+			mw.toEdit.SetText(mw.settings.Items[i].To)
+			return
+		}
+	}
+}
+
 func main() {
+	mw := new(MyMainWindow)
+
 	if _, err := (MainWindow{
-		Title:   "RegExp Replace",
-		MinSize: Size{10, 30},
-		Layout:  VBox{},
+		AssignTo: &mw.MainWindow,
+		Title:    "RegExp Replace",
+		MinSize:  Size{400, 200},
+		Layout:   VBox{},
 		OnDropFiles: func(files []string) {
 		},
 		Children: []Widget{
@@ -52,25 +74,30 @@ func main() {
 						Text: "Find what:",
 					},
 					LineEdit{
-						Text: Bind("From"),
+						AssignTo: &mw.fromEdit,
 					},
 
 					Label{
 						Text: "Replace with:",
 					},
 					LineEdit{
-						Text: Bind("To"),
+						AssignTo: &mw.toEdit,
 					},
 
 					Label{
 						Text: "Settings:",
 					},
 					ComboBox{
-						Value:         Bind("Setting", SelRequired{}),
 						DisplayMember: "Name",
-						Model:         getSettings(),
+						Model:         mw.getSettings(),
+						OnCurrentIndexChanged: mw.setSettings,
+						AssignTo:              &mw.nameBox,
 					},
 				},
+			},
+			PushButton{
+				Text:      "Save Settings",
+				OnClicked: func() { mw.saveSettingsDialog(mw.MainWindow) },
 			},
 		},
 	}.Run()); err != nil {
@@ -78,5 +105,121 @@ func main() {
 	}
 }
 
-func (mw *MyMainWindow) openAction_Triggered() {
+func (mw *MyMainWindow) saveSettingsDialog(owner walk.Form) (int, error) {
+	if mw.fromEdit.Text() == "" || mw.toEdit.Text() == "" {
+		mw.errorDialog(owner, "Invalid value.")
+	}
+
+	var dlg *walk.Dialog
+	var acceptPB, cancelPB *walk.PushButton
+
+	return Dialog{
+		AssignTo:      &dlg,
+		Title:         "Set setting name",
+		DefaultButton: &acceptPB,
+		CancelButton:  &cancelPB,
+		MinSize:       Size{300, 100},
+		Layout:        VBox{},
+		Children: []Widget{
+			Composite{
+				Layout: Grid{Columns: 2},
+				Children: []Widget{
+					Label{
+						Text: "Name:",
+					},
+					LineEdit{
+						AssignTo: &mw.nameEdit,
+					},
+				},
+			},
+			Composite{
+				Layout: HBox{},
+				Children: []Widget{
+					PushButton{
+						AssignTo: &acceptPB,
+						Text:     "OK",
+						OnClicked: func() {
+							if mw.nameEdit.Text() == "" {
+								mw.errorDialog(owner, "Invalid name.")
+								return
+							} else {
+								mw.saveSettings()
+								mw.nameBox.SetModel(mw.settings.Items)
+								dlg.Accept()
+								return
+							}
+						},
+					},
+					PushButton{
+						AssignTo:  &cancelPB,
+						Text:      "Cancel",
+						OnClicked: func() { dlg.Cancel() },
+					},
+				},
+			},
+		},
+	}.Run(owner)
+}
+
+func (mw *MyMainWindow) errorDialog(owner walk.Form, msg string) (int, error) {
+	var dlg *walk.Dialog
+	return Dialog{
+		AssignTo: &dlg,
+		Title:    "Error",
+		MinSize:  Size{200, 80},
+		Layout:   VBox{},
+		Children: []Widget{
+			Composite{
+				Layout: VBox{},
+				Children: []Widget{
+					Label{
+						Text: msg,
+					},
+					PushButton{
+						Text: "OK",
+						OnClicked: func() {
+							dlg.Accept()
+							return
+						},
+					},
+				},
+			},
+		},
+	}.Run(owner)
+}
+
+func (mw *MyMainWindow) saveSettings() {
+	var item = Item{
+		mw.nameEdit.Text(),
+		mw.fromEdit.Text(),
+		mw.toEdit.Text(),
+	}
+	var flag bool = true
+	for i, v := range mw.settings.Items {
+		if v.Name == item.Name {
+			flag = false
+			mw.settings.Items[i].From = item.From
+			mw.settings.Items[i].To = item.To
+			break
+		}
+	}
+	if flag {
+		mw.settings.Items = append(mw.settings.Items, &item)
+	}
+
+	fp, err := os.OpenFile("config.yml", os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer fp.Close()
+	writer := bufio.NewWriter(fp)
+	yml, err := yaml.Marshal(mw.settings)
+	if err != nil {
+		log.Fatal(err)
+	}
+	_, err = writer.WriteString(string(yml))
+	if err != nil {
+		log.Fatal(err)
+	}
+	writer.Flush()
 }
